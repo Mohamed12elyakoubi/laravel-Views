903871d5eca6f935af701ae414ee96e1fa44b62b
mohamed23-2000 <elyakoubi412@gmail.com>
2023-10-17 12:13:53 +0200

    cc

---
+++ example/resources/views/test.blade.php
@@ -0,0 +1,7 @@
+@foreach ($planets as $planet)
+
+    <h2>{{ $planet['name'] }}</h2>
+
+    {{ $planet['description'] }}
+
+@endforeach
--- example/routes/web.php
+++ example/routes/web.php
@@ -6,11 +6,11 @@ Route::get('/test', function () {
     return  ["Uranus", "Jupiter", "Mars", "Aarde", "Saturnus", "Pluto", "Neptunus", "Venus"];
 });
 
-Route::get('/', function() {
-    return view("welcome"); 
+Route::get('/', function () {
+    return view("welcome");
 });
 
-Route::get('/gg', function() {
+Route::get('/gg', function () {
 
     $planets = [
         'Mars' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.',
@@ -21,25 +21,54 @@ Route::get('/gg', function() {
 
 
     return view('test', ['planets' => $planets]);
-
 });
-    
+
 Route::get('/planets', function () {
-    $allPlanets =[
-        'Mars' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.',
-        'Venus' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.',
-        'Earth' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that is inhabited by living things.',
-        'Jupiter' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+
+    $planets = [
+
+        [
+
+            'name' => 'Mars',
+
+            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
+
+        ],
+
+        [
+
+            'name' => 'Venus',
+
+            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
+
+        ],
+
+        [
+
+            'name' => 'Earth',
+
+            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
+
+        ],
+        [
+
+            'name' => 'Jupiter',
+
+            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
+
+        ],
+
     ];
+    $selectedPlanet = request('planeet');
 
-    $planetName = request('planet');
-
-    if ($planetName) {
-        
-        $planets = [$planetName => $allPlanets[$planetName]];
+    if ($selectedPlanet) {
+        $filteredPlanets = collect($planets)->where('name', $selectedPlanet)->all();
     } else {
-        $planets = $allPlanets;
+
+        $filteredPlanets = $planets;
     }
 
-    return view('planets', ['planets' => $planets]);
+
+
+    return view('test')->with(['planets' => $filteredPlanets]);
 });
