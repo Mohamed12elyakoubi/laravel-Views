b6bcbb21becfb9da39a4dd4553bf16b6461ac3d1
mohamed23-2000 <elyakoubi412@gmail.com>
2023-12-05 12:12:23 +0100

    gwn

--- example/app/Http/Controllers/PlanetController.php
+++ example/app/Http/Controllers/PlanetController.php
@@ -1,38 +1,23 @@
 <?php
 
+// app\Http\Controllers\PlanetController.php
+
 namespace App\Http\Controllers;
 
+use App\Models\Planet;
 use Illuminate\Http\Request;
 
 class PlanetController extends Controller
 {
-    private $planets = [
-        [
-            'name' => 'Mars',
-            'description' => 'Mars is the fourth planet from the Sun and the second-smallest planet in the Solar System, being larger than only Mercury.'
-        ],
-        [
-            'name' => 'Venus',
-            'description' => 'Venus is the second planet from the Sun. It is named after the Roman goddess of love and beauty.'
-        ],
-        [
-            'name' => 'Earth',
-            'description' => 'Our home planet is the third planet from the Sun, and the only place we know of so far that\'s inhabited by living things.'
-        ],
-        [
-            'name' => 'Jupiter',
-            'description' => 'Jupiter is a gas giant and doesn\'t have a solid surface, but it may have a solid inner core about the size of Earth.'
-        ],
-    ];
-
     public function index()
     {
-        return view('planets.index', ['planets' => $this->planets]);
+        $planets = Planet::all();
+        return view('planets.index', ['planets' => $planets]);
     }
 
     public function show($planet)
     {
-        $selectedPlanet = collect($this->planets)->firstWhere('name', $planet);
+        $selectedPlanet = Planet::where('name', $planet)->first();
 
         if ($selectedPlanet) {
             return view('planets.show', ['planet' => $selectedPlanet]);
